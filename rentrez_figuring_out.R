63a6fbf29191be9b197e03e70ac4d1fe4d6faddd
slcornett <slcornett@utexas.edu>
2022-04-29 14:02:30 -0500

    cut out the middle man, sequences are in the csv

--- rentrez_figuring_out.R
+++ rentrez_figuring_out.R
@@ -6,15 +6,13 @@ require(devtools)
 require(roxygen2)
 require(withr)
 
-f <- "https://raw.githubusercontent.com/jyhrehjohnson/cornett-johnson_ADA_GroupProject/main/SHH_orthologs.csv"
+f <- "https://raw.githubusercontent.com/jyhrehjohnson/cornett-johnson_ADA_GroupProject/main/SHH_orthologs_1.csv"
 d <- read_csv(f, col_names = TRUE)
-
-SHH_search <- entrez_search(db="gene",
-                            term ="(SHH[All Fields] AND (Gnathostomata [Organism] OR Gnathostomata [Organism] OR Gnathostomata[All Fields]) AND orthologs[All Fields]) AND alive[prop]", 
-                            use_history=TRUE)
-SHH_search$web_history 
-
-SHH_aa.seq <- entrez_fetch(db = "sequences", rettype = "fasta", web_history = SHH_search$web_history)
-
+SHH_aa.seq <- entrez_fetch(db = "protein", 
+                           id = rowwise(d$RefSeq_Protein_accessions), 
+                           rettype = "fasta", 
+                          # web_history = SHH_search$web_history
+                           )
+SHH_aa.seq
 sequences <- 
-d <- d %>% mutate(entrez_fetch())
+d <- d %>% mutate(SHH_Peptides = SHH_aa.seq)
