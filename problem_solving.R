88f4393dfccc2908f37843e84054c7ec85b92d6a
slcornett <slcornett@utexas.edu>
2022-05-07 19:50:57 -0500

    added Toparslan et al. 2020 reference and 1 package (msa)

--- problem_solving.R
+++ problem_solving.R
@@ -9,10 +9,7 @@ require(withr)
 
 f <- "https://raw.githubusercontent.com/jyhrehjohnson/cornett-johnson_ADA_GroupProject/main/SHH_orthologs_Carnivora.csv"
 d <- read_csv(f, col_names = TRUE)
-#SHH_aa.seq <- entrez_fetch(db = "protein", 
-                          # id = d$RefSeq_Protein_accessions, 
-                          # rettype = "fasta")
-                          # web_history = SHH_search$web_history
+
 
 
 
