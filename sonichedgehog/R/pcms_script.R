aad69120a32512858b60eb287a865f8775cf8240
= <=>
2022-05-08 22:02:51 -0500

    Updated/Created functions

--- sonichedgehog/R/pcms_script.R
+++ sonichedgehog/R/pcms_script.R
@@ -19,20 +19,26 @@ read_file <- function(f_name){
   return(data)
 }
 
+f_file <- function(fast_file){
+  #for reading multiple AA sequences from msa package
+  fast <-readAAStringSet(fast_file, format = "fasta", use.names = TRUE) # format: biostrings, AAString set
+  return(fast)
+}
+
 # built-in data set: SHH_Carnivora.csv (Carnivore orthologs)
 #SHH_Carn <- "https://raw.githubusercontent.com/jyhrehjohnson/cornett-johnson_ADA_GroupProject/main/SHH_orthologs_Carnivora.csv"
 
 # building a function to make a polypeptide (Amino Acid) sequence phylogenetic tree from data in fasta file
-pcms_AAtree <- function(f_name){
+pcms_AAtree <- function(fast_file){
   #for reading multiple AA sequences from msa package
-  fas <- readAAStringSet(f_name, format = "fasta", use.names = TRUE)
+  #fas <- readAAStringSet(fas_file, format = "fasta", use.names = TRUE)
   #align the fasta file using MUSCLE algorithm: multiple sequence alignment from msa package 
-  msa(fas, method = c("Muscle"), type = "protein", order=c("aligned", "input"))
+  msa(fast, method = c("Muscle"), type = "protein", order=c("aligned", "input"))
   #read aligned data, storing in AAbin format (class will be AAbin) (ape package)
-  ape::as.AAbin(fas, show.aa = TRUE, check.names = TRUE)
-  ape::as.character.AAbin(fas) #converting AAbin data to character format, 
-  ape:: as.alignment(fas) # # converting from AAbin to alignment format for phylogenetic tree
-  base::as.matrix(fas) # converting alignment to matrix
+  ape::as.AAbin(fast, show.aa = TRUE, check.names = TRUE)
+  ape::as.character.AAbin(fast) #converting AAbin data to character format, 
+  ape:: as.alignment(fast) # # converting from AAbin to alignment format for phylogenetic tree
+  base::as.matrix(fast) # converting alignment to matrix
   # talk to tony about fxn because piping the separate functions together doesn't work, 
   # worried this fxn won't work for the same reason
 }
@@ -52,13 +58,9 @@ ggt <-ggtree(tree, cex = 0.8, aes(color=branch.length)) +
 ggt
 
 
-
 #Plot the maximum likelihood
 plot_Tree <- function(tree){
 plotTree(tree, fsize=0.8,lwd=1,offset=1) #Plot the ML, set font size, create space so nodes aren't on top of each other 
-nodelabels(tree$node.label, adj = c(1, 0), frame = "none")
-return(plot_Tree)
+nodelabels(tree$node.label, adj = c(1, 0), frame = "none") #Label the nodes
 }
 
-plotTree(tree, fsize=0.8,lwd=1,offset=1) #Plot the ML, set font size, create space so nodes aren't on top of each other 
-nodelabels(tree$node.label, adj = c(1, 0), frame = "none") #label the nodes
